6d8e3dd353f52b893e3bb3509a9f561caa923902
Kenji Okimoto <okimoto@clear-code.com>
2018-02-28 18:03:21 +0900

    Fix test

--- test/handlers/feedback/test-github.rb
+++ test/handlers/feedback/test-github.rb
@@ -23,13 +23,15 @@ class TestGitHub < Test::Unit::TestCase
 
   data(issues: "https://github.com/okkez/pragmatic-bot/issues/1 find okkez/pragmatic-bot",
        pull: "https://github.com/okkez/pragmatic-bot/pull/1 find okkez/pragmatic-bot",)
-  test "issues or pull w/ garbage" do |url|
+  test "issues or pull w/ action and repository" do |url|
     original = {
       body: "@ruboty #{url}",
       from: "alice",
       to: "#general"
     }
-    mock(Ruboty::Actions::Github).new(anything, "dummy", "dummy", "dummy").times(0)
+    mock(Ruboty::Actions::Github).new(anything, "dummy", "dummy", "dummy") do
+      lambda { }
+    end
     @robot.receive(original)
     assert(true)
   end
